445dfea6cb47e89229be67ad664d5cea38e4d2fd
unknown <gebizar@uw.edu>
2019-05-07 12:12:09 -0700

    Sticky Scrolling

---
+++ resources/js/index.js
@@ -0,0 +1,4 @@
+$(function() {
+    $(".nav-div").css({"width": $(".header-image").width() + 'px'});
+    $(".header-sticky-container").css({"margin-top": -$(".header-sticky-container").height() + "px"});
+});
