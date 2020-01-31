3366379688d0c5977e6a1f928c0148fe0615f4bf
Gabe <gebizar@uw.edu>
2020-01-30 16:05:41 -0800

    90 percent completed

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,4 +1,6 @@
 $(function() {
+    // $("#main-content").css({"height": $("window").screen.height() - $("footer").height()});
+    // $("#main-content").css({"width": $("header").width()});
     $(".nav-div").css({"width": $(".header-image").width() + 'px'}).css({"margin-left": $(".header-image").offset().left + "px"})
     // $(".header-sticky-container").css({"margin-top": -$(".header-sticky-container").height() + "px"});
     $(".nav-sections").css({"margin-top": $(".header-sticky-container").height() / 4 + "px"})
@@ -7,7 +9,17 @@ $(function() {
     resizeText();
     $(".header-sticky-container").css({"margin-top": -$(".header-sticky-container").height() + "px"});
 
-    var setfontSize = $(".header-image").width()  
+   
+    $(".setimage").css({"width": "100%"});
+
+
+    var imageSize = $(".setimage").width()  
+    // let sectionBuffer = $(window).height() / 6;
+    console.log(imageSize);
+    $(".to-set").css({"width": imageSize + "px"});
+    $(".nav-sections").css({"margin-bottom": $("footer").height()});
+    // $(".to-set").css({"max-height": Math.round(9/16 * imageSize) + "px"})
+    
 
     $('#main-nav li a').click(function(e) {
         var targetHref = $(this).attr('href');
@@ -26,8 +38,8 @@ $(function() {
         var opacity=0;
         var windowTop = $(window).scrollTop();
         var windowBottom = windowTop + $(window).height();
+        let currSec = 0;
         if( offset<=fadeStart ){
-            
             opacity=1;
         }else if( offset<=fadeUntil ){
             opacity=1-offset/fadeUntil;
@@ -37,10 +49,21 @@ $(function() {
         $(".nav-sections").children().each(function(){
             var childTop = $(this).offset().top
             var childBottom = childTop + $(this).outerHeight();
-            if ((childBottom <= windowBottom) && (childTop >= windowTop)) {
-                $(this).addClass("in-view");
-            } else {
+            // var childBottom = childTop + 175;
+
+            // if ((childBottom <= windowBottom) && (childTop >= windowTop)) {
+            //     console.log($(this).attr("id"))
+            //     $(this).addClass("in-view");
+            // } else {
+            //     $(this).removeClass("in-view");
+            // }
+            var sectionBuffer = $(this).outerHeight() / 4;
+            console.log($(this).attr("id"))
+            console.log(childTop, " ", childBottom, " ", windowTop, " ", windowBottom);
+            if ((childBottom - sectionBuffer < windowTop) || (childTop + sectionBuffer > windowBottom) || windowTop == 0) {
                 $(this).removeClass("in-view");
+            } else {
+                $(this).addClass("in-view");
             }
         })
     });
