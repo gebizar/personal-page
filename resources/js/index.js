acc742dce3953db46825e6c6831751fe9508f2b1
Gabe <gebizar@uw.edu>
2020-02-06 11:13:37 -0800

    nearly done

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,25 +1,16 @@
 $(function() {
-    // $("#main-content").css({"height": $("window").screen.height() - $("footer").height()});
-    // $("#main-content").css({"width": $("header").width()});
     $(".nav-div").css({"width": $(".header-image").width() + 'px'}).css({"margin-left": $(".header-image").offset().left + "px"})
-    // $(".header-sticky-container").css({"margin-top": -$(".header-sticky-container").height() + "px"});
     $(".nav-sections").css({"margin-top": $(".header-sticky-container").height() / 4 + "px"})
     $(".header-text").css({"margin-left": $(".header-image").offset().left + "px"})
     $(".header-subtext").css({"margin-left": $(".header-image").offset().left + "px"})
     resizeText();
+    
     $(".header-sticky-container").css({"margin-top": -$(".header-sticky-container").height() + "px"});
-
-   
     $(".setimage").css({"width": "100%"});
 
-
     var imageSize = $(".setimage").width()  
-    // let sectionBuffer = $(window).height() / 6;
-    console.log(imageSize);
     $(".to-set").css({"width": imageSize + "px"});
     $(".nav-sections").css({"margin-bottom": $("footer").height()});
-    // $(".to-set").css({"max-height": Math.round(9/16 * imageSize) + "px"})
-    
 
     $('#main-nav li a').click(function(e) {
         var targetHref = $(this).attr('href');
@@ -49,17 +40,7 @@ $(function() {
         $(".nav-sections").children().each(function(){
             var childTop = $(this).offset().top
             var childBottom = childTop + $(this).outerHeight();
-            // var childBottom = childTop + 175;
-
-            // if ((childBottom <= windowBottom) && (childTop >= windowTop)) {
-            //     console.log($(this).attr("id"))
-            //     $(this).addClass("in-view");
-            // } else {
-            //     $(this).removeClass("in-view");
-            // }
             var sectionBuffer = $(this).outerHeight() / 4;
-            console.log($(this).attr("id"))
-            console.log(childTop, " ", childBottom, " ", windowTop, " ", windowBottom);
             if ((childBottom - sectionBuffer < windowTop) || (childTop + sectionBuffer > windowBottom) || windowTop == 0) {
                 $(this).removeClass("in-view");
             } else {
@@ -69,19 +50,11 @@ $(function() {
     });
 
     function resizeText() {
-        console.log($(window).width())
         var picWidth = $(".header-image").width()
-        console.log("Pic Width", picWidth);
         var resizeText = Math.round(picWidth / (960 / 50)) + "px";
         var resizeSubtext = Math.round(picWidth / (960 / 30)) + "px";
-        console.log(resizeText);
         $(".header-text").css({"font-size": resizeText}) 
         $(".header-subtext").css({"font-size": resizeSubtext})
     }
-
     $(window).resize(resizeText);
-
-   
-
-    
 });
